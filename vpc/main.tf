c0015c7157c7bf2ce6896fed402ab1c2d8c53f6e
Sujad Syed <trainer@titansystems.org>
2026-02-11 21:46:33 +0530

    main

--- vpc/main.tf
+++ vpc/main.tf
@@ -19,7 +19,7 @@ resource "google_compute_network" "vpc" {
 
 resource "google_compute_subnetwork" "subnet" {
   name          = "finops-subnet"
-  ip_cidr_range = "10.10.2.0/24"
+  ip_cidr_range = "10.20.1.0/24"
   region        = var.region
   network       = google_compute_network.vpc.id
 }
